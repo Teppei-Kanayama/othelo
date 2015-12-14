20fb1262906a1b7c8a8f2e37b6c5bed2abf7a747
Teppei Kanayama <1994teppei0615kanayama@gmail.com>
2015-12-14 22:35:03 +0900

    arduino codes was modified

--- arduino_code/main_program/main_program.ino
+++ arduino_code/main_program/main_program.ino
@@ -1,13 +1,16 @@
 #include <Servo.h>
+#include <math.h>
 
-char nums[6];
-int i = 0;
-int val[2][2];
+int val[16];
 Servo servo[2];
 int CATCH_PIN = 6, RELEASE_PIN = 7;
 
 int home_pos[2] = {
-  5, 5
+  0, 0
+};
+
+int release_pos[2] = {
+  170, 90
 };
 
 void setup(){
@@ -19,29 +22,22 @@ void setup(){
 }
 
 void loop(){
-  
-  digitalWrite(CATCH_PIN, LOW);
+  int data_num;
+  digitalWrite(CATCH_PIN,  LOW);
   digitalWrite(RELEASE_PIN, LOW);
   servo[0].write(home_pos[0]);
   servo[1].write(home_pos[1]);
   
-  if(Serial.available()){
-    
-    nums[i] = Serial.read() - '0';
-    i++;
-    if(i > 5) {
-      i = 0;
-      val[0][0] = 100 * nums[0] + 10 * nums[1] + 1 * nums[2];
-      val[0][1] = 100 * nums[3] + 10 * nums[4] + 1 * nums[5];
-      Serial.println(val[0][0]);
-      Serial.println(val[0][1]);
-      
+  if(data_num = Serial.available()){
+    for(int i = 0; i < data_num; i++){
+      val[i%3] += (Serial.read() - '0') * pow(10, 2 - i%3);  
+    }
       digitalWrite(CATCH_PIN, HIGH);
       digitalWrite(RELEASE_PIN, LOW);
       delay(1000);
       
-      servo[0].write(val[0][0]);
-      servo[1].write(val[0][1]);
+      servo[0].write(val[0]);
+      servo[1].write(val[1]);
       delay(500);
       
       digitalWrite(CATCH_PIN, LOW);
@@ -50,9 +46,22 @@ void loop(){
       
       digitalWrite(CATCH_PIN, LOW);
       digitalWrite(RELEASE_PIN, LOW);
-      servo[0].write(home_pos[0]);
-      servo[1].write(home_pos[1]);
-      delay(500);
-    }
+      
+      for(int i = 2; i < data_num % 6; i += 2){
+        servo[0].write(val[i]);
+        servo[1].write(val[i+1]);
+        delay(500);
+        digitalWrite(CATCH_PIN, HIGH);
+        digitalWrite(RELEASE_PIN, LOW);
+        delay(3000);
+        servo[0].write(release_pos[0]);
+        servo[1].write(release_pos[1]);
+        delay(500);
+        digitalWrite(CATCH_PIN, HIGH);
+        digitalWrite(RELEASE_PIN, LOW);
+        delay(1000);
+        digitalWrite(CATCH_PIN, LOW);
+        digitalWrite(RELEASE_PIN, LOW);
+      }
   }
 }
---
+++ arduino_code/test_program/test_program.ino
@@ -0,0 +1,58 @@
+#include <Servo.h>
+
+char nums[6];
+int i = 0;
+int val[2][2];
+Servo servo[2];
+int CATCH_PIN = 6, RELEASE_PIN = 7;
+
+int home_pos[2] = {
+  5, 5
+};
+
+void setup(){
+   Serial.begin(9600);
+   servo[0].attach(8);
+   servo[1].attach(9);
+   pinMode(CATCH_PIN, OUTPUT);
+   pinMode(RELEASE_PIN, OUTPUT);
+}
+
+void loop(){
+  
+  digitalWrite(CATCH_PIN, LOW);
+  digitalWrite(RELEASE_PIN, LOW);
+  servo[0].write(home_pos[0]);
+  servo[1].write(home_pos[1]);
+  
+  if(Serial.available()){
+    
+    nums[i] = Serial.read() - '0';
+    i++;
+    if(i > 5) {
+      i = 0;
+      val[0][0] = 100 * nums[0] + 10 * nums[1] + 1 * nums[2];
+      val[0][1] = 100 * nums[3] + 10 * nums[4] + 1 * nums[5];
+      Serial.println(val[0][0]);
+      Serial.println(val[0][1]);
+      
+      digitalWrite(CATCH_PIN, HIGH);
+      digitalWrite(RELEASE_PIN, LOW);
+      delay(1000);
+      
+      servo[0].write(val[0][0]);
+      servo[1].write(val[0][1]);
+      delay(500);
+      
+      digitalWrite(CATCH_PIN, LOW);
+      digitalWrite(RELEASE_PIN, HIGH);
+      delay(3000);
+      
+      digitalWrite(CATCH_PIN, LOW);
+      digitalWrite(RELEASE_PIN, LOW);
+      servo[0].write(home_pos[0]);
+      servo[1].write(home_pos[1]);
+      delay(500);
+    }
+  }
+}
