7b528baabb4089f97a96da1e0105e4170fc62415
Teppei Kanayama <1994teppei0615kanayama@gmail.com>
2015-12-16 14:15:41 +0900

    othero coord was modified.

--- my_ik.py
+++ my_ik.py
@@ -24,13 +24,15 @@ def inverse_kinematics(x, y, ox, oy, l1, l2):
 def calc_coord(x, y, x1, y1, x2, y2): #グローバル座標系におけるオセロの端点(x1, y1), (x2, y2)をもとに、オセロ座標系(x, y)(x = 0~7, y = 0~7)をグローバル座標系に変換
     e1 = np.array([x2 - x1, y2 - y1]) / 8
     e2 = np.array([[0, 1], [-1, 0]]).dot(e1) / 8
-    return np.array([x1, y1]) + (x + 0.5) * e1 + (y + 0.5) * e2
-
+    #return np.array([x1, y1]) + (x + 0.5) * e1 + (y + 0.5) * e2
+    return np.array([x1, y1]) + (y + 0.5) * e1 + (x + 0.5) * e2  
+    
 def pos_to_arg(pos_list, x1, y1, x2, y2, l1, l2): #グローバル座標系の原点はアームの原点に一致させておく
     ans = []
     for pos in pos_list:
         tmp = calc_coord(pos[0], pos[1], x1, y1, x2, y2)
         ans.append(inverse_kinematics(tmp[0], tmp[1], 0, 0, l1, l2))
     return ans
+
 #print calc_coord(0, 0, 0, 10, 10, 10)
 #print inverse_kinematics(0, 20, 0, 0, 10, 10)
